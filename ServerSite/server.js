96e04c1692bd0ac9117ffd2c55fb8ca3588e3c89
bilalxD <pbilal75@gmail.com>
2024-02-08 22:48:20 +0500

    update karo jaldi

--- ServerSite/server.js
+++ ServerSite/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "./Routes/auth.routes.js"
+import authRoutes from "./Routes/auth.routes.js";
 
 const app = express();
 
