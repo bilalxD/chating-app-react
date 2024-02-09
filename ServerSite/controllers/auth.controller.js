c844941c864cca006cbb9001632f3a32a8817657
bilalxD <pbilal75@gmail.com>
2024-02-09 15:14:12 +0500

    message

---
+++ ServerSite/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const login = (req, res) =>{
+    res.send("This is our login api.")
+}
+
+export const logout = (req, res) =>{
+    res.send("This is our logout api.")
+}
+
+export const signup = (req, res) =>{
+    res.send("This is our signup api.")
+}
