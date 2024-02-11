9658232f11cf5b86ed41985de054bfa59a68d23d
bilalxD <pbilal75@gmail.com>
2024-02-12 03:55:52 +0500

    galti hogai thi

--- ServerSite/server.js
+++ ServerSite/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./Routes/auth.routes.js";
 import DB from "./db/database.js";
-
+import User from "./models/user.model.js"
 
 const app = express();
 const PORT = process.env.PORT || 5000;
