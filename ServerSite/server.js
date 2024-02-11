a6cbe1fd0df585eda38ef552318f229590b4265e
bilalxD <pbilal75@gmail.com>
2024-02-12 03:50:52 +0500

    asc

--- ServerSite/server.js
+++ ServerSite/server.js
@@ -2,17 +2,19 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./Routes/auth.routes.js";
 import DB from "./db/database.js";
+
+
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
-
-app.get("/", (req, res) => {
-  res.json("hello World");
-});
-
 app.use("/api/auth", authRoutes);
+
+
+// app.get("/", (req, res) => {
+//   res.json("hello World");
+// });
 
 app.listen(PORT, () => {
   DB();
