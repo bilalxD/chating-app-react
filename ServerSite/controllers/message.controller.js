4fec93f627425e16af58be8334df3141a328ac11
bilalxD <pbilal75@gmail.com>
2024-02-19 04:49:26 +0500

    messages

--- ServerSite/controllers/message.controller.js
+++ ServerSite/controllers/message.controller.js
@@ -32,3 +32,19 @@ try {
     res.status(500).json({error: "Internal Server Error"});
 }
 }
+
+
+export const getMessage = async (req,res) =>{
+    try {
+        const {id: receiverChat} = req.body;
+        const senderId = req.user._id;
+
+        const conversation = await Conversation.findOne({
+            participants: {$all: [senderId,receiverId]}.populate("messages");
+        })
+        res.status(200).json(conversation.messages;)
+    } catch (error) {
+        console.log("Error In sendMessage Controller,",error );
+    res.status(500).json({error: "Internal Server Error"});
+    }
+}
